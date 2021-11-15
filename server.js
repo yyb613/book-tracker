506055cc90a0417dc807921ee7d33b0e8c618b78
Roger Le <rogermle@gmail.com>
2021-11-15 13:11:05 -0500

    Adding handlebars config to server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { sequelize } = require('./models');
 const db = require('./models');
+const hbs =  require('express-handlebars');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.engine('handlebars', hbs.engine());
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
     res.send("Homepage!");
