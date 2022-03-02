c4386419b5928cd37d355acf06926ff257b7175e
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-03-02 14:28:32 -0500

    created sql schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,32 @@
-DROP DATABASE IF EXISTS example_db;
-CREATE DATABASE example_db;
+DROP DATABASE IF EXISTS my_library_db;
+CREATE DATABASE my_library_db;
+
+USE DATABASE my_library_db;
+
+CREATE TABLE `AddedBook` (
+    `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(50)  NOT NULL ,
+    `author` VARCHAR(50)  NOT NULL ,
+    `numPages` VARCHAR(50)  NOT NULL ,
+);
+
+CREATE TABLE `AlreadyRead` (
+    `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(50)  NOT NULL ,
+    `author` VARCHAR(50)  NOT NULL ,
+    `numPages` VARCHAR(50)  NOT NULL ,
+);
+
+CREATE TABLE `CurrentlyReading` (
+    `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(50)  NOT NULL ,
+    `author` VARCHAR(50)  NOT NULL ,
+    `numPages` VARCHAR(50)  NOT NULL ,
+);
+
+CREATE TABLE `WantToRead` (
+    `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(50)  NOT NULL ,
+    `author` VARCHAR(50)  NOT NULL ,
+    `numPages` VARCHAR(50)  NOT NULL ,
+);
