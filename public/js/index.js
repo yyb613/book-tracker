bf1b5a93cecec79616ec4b18cfa9eb106de7c62f
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-03-03 13:36:42 -0500

    created card deck

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,8 @@
 console.log("Test");
+
+$(document).ready(function () {
+    $(".nav-link").click(function (e) {
+        $(".nav-link").removeClass("activeNav");
+        $(this).addClass("activeNav");
+    });
+  });
