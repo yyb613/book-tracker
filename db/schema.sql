757724db88ef16ea9867534a42fee0b1aebf88fe
Steven Kravitz <Steven.Kravitz95@gmail.com>
2022-03-03 10:35:34 -0500

    removed commas from schema to make schema bug free

--- db/schema.sql
+++ db/schema.sql
@@ -7,26 +7,26 @@ CREATE TABLE `AddedBook` (
     `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(50)  NOT NULL ,
     `author` VARCHAR(50)  NOT NULL ,
-    `numPages` VARCHAR(50)  NOT NULL ,
+    `numPages` VARCHAR(50)  NOT NULL
 );
 
 CREATE TABLE `AlreadyRead` (
     `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(50)  NOT NULL ,
     `author` VARCHAR(50)  NOT NULL ,
-    `numPages` VARCHAR(50)  NOT NULL ,
+    `numPages` VARCHAR(50)  NOT NULL
 );
 
 CREATE TABLE `CurrentlyReading` (
     `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(50)  NOT NULL ,
     `author` VARCHAR(50)  NOT NULL ,
-    `numPages` VARCHAR(50)  NOT NULL ,
+    `numPages` VARCHAR(50)  NOT NULL 
 );
 
 CREATE TABLE `WantToRead` (
     `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(50)  NOT NULL ,
     `author` VARCHAR(50)  NOT NULL ,
-    `numPages` VARCHAR(50)  NOT NULL ,
+    `numPages` VARCHAR(50)  NOT NULL
 );
